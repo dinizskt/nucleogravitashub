423519bbd56c1cadd7a2172467b7078143332309
Cassio Diniz <c4ssio180@gmail.com>
2026-08-25 23:51:31 -0300

    Initial commit: Hub Núcleo Gravitas
    Hub interno de ofertas lowticket com banco de 44 ofertas mineradas,
    notas de demanda/concorrência pesquisadas, e módulos adicionais
    (mecanismos, headlines, calculadora de esteira etc.) estruturados
    para lançamento futuro.

---
+++ serve.js
@@ -0,0 +1,32 @@
+const http = require("http");
+const fs = require("fs");
+const path = require("path");
+
+const root = __dirname;
+const types = {
+  ".html": "text/html; charset=utf-8",
+  ".css": "text/css; charset=utf-8",
+  ".js": "application/javascript; charset=utf-8",
+  ".png": "image/png",
+  ".jpg": "image/jpeg",
+  ".jpeg": "image/jpeg",
+  ".svg": "image/svg+xml",
+};
+
+http
+  .createServer((req, res) => {
+    let urlPath = decodeURIComponent(req.url.split("?")[0]);
+    if (urlPath === "/") urlPath = "/index.html";
+    const filePath = path.join(root, urlPath);
+    fs.readFile(filePath, (err, data) => {
+      if (err) {
+        res.writeHead(404);
+        res.end("Not found");
+        return;
+      }
+      const ext = path.extname(filePath).toLowerCase();
+      res.writeHead(200, { "Content-Type": types[ext] || "application/octet-stream" });
+      res.end(data);
+    });
+  })
+  .listen(8099, () => console.log("Serving on http://localhost:8099"));
